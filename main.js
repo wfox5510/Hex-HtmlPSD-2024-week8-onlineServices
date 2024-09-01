ec486dc4f5bcb99683f938c34315d853e843bca8
wfox5510 <wfox5510@gmail.com>
2024-09-01 21:18:15 +0800

    complete banner / nav section

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-console.log("Hello world!");
+var swiper = new Swiper(".bannerSwiper", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 30,
+  loop: true,
+  loopFillGroupWithBlank: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    320: {  //当屏幕宽度大于等于320
+
+    },
+    768: {  //当屏幕宽度大于等于768 
+
+    },
+    1200: {  //当屏幕宽度大于等于1280
+      navigation: {
+        nextEl: ".bannerSwiper-next",
+        prevEl: ".bannerSwiper-prev",
+      },
+    }
+  }
+});
